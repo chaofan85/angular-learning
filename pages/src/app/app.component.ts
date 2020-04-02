0492f9a689ec206091d0f8b930d6e4591ba23e1a
chaofan85 <fanchao05@gmail.com>
2020-04-02 18:39:00 -0400

    set up pagination

--- pages/src/app/app.component.ts
+++ pages/src/app/app.component.ts
@@ -8,6 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   currentPage = 0;
 
+  checkPageIndex(i: number) {
+    return Math.abs(this.currentPage - i) < 5;
+  }
+
   images = [
     {
       title: 'At the Beach',
