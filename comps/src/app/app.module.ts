785f67347ea12eadc397ebfe574d6873060e06a1
chaofan85 <fanchao05@gmail.com>
2020-04-03 18:17:51 -0400

    created new project

---
+++ comps/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ElementsModule } from './elements/elements.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ElementsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
