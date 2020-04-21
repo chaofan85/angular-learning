6514df56dbc6823cb59f5de7c3f5ec84c6c5ed75
chaofan85 <fanchao05@gmail.com>
2020-04-21 16:21:47 -0400

    created new project

--- comps/src/app/mods/mods-home/mods-home.component.ts
+++ comps/src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
   modalOpen = false;
+  items = [
+    {
+      title: 'Why is the sky blue?',
+      content: 'The sky is blue because it is.',
+    },
+    { title: 'What does an orange taste like?', content: 'Sweet and sour.' },
+    {
+      title: 'What color is that cat?',
+      content: 'The cat is an orange color.',
+    },
+  ];
 
   constructor() {}
 
