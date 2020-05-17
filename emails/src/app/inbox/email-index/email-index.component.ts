b4bdbcf94b8c59ff8705139dce8d9d7f2c984035
chaofan85 <fanchao05@gmail.com>
2020-05-17 17:24:21 -0400

    updated email index page

--- emails/src/app/inbox/email-index/email-index.component.ts
+++ emails/src/app/inbox/email-index/email-index.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-index',
   templateUrl: './email-index.component.html',
-  styleUrls: ['./email-index.component.css']
+  styleUrls: ['./email-index.component.css'],
 })
 export class EmailIndexComponent implements OnInit {
-
-  constructor() { }
+  emails = [];
+  constructor(private emailService: EmailService) {}
 
   ngOnInit(): void {
+    this.emailService.getEmails().subscribe((emails) => {
+      this.emails = emails;
+    });
   }
-
 }
